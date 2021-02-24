82a6e8c1c3851d165cf45ab619c2b329e2ebaefa
giannurwana19 <giannurwana19@gmail.com>
2021-02-24 21:31:53 +0700

    display all posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,32 @@
         Posts
         <a href="{{ route('posts.create') }}" class="btn btn-sm btn-primary float-right">+ Post</a>
     </div>
-    <div class="card-body"></div>
+    <div class="card-body">
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($posts as $post)
+                <tr>
+                    <td><img src="{{ asset("storage/$post->image") }}" width="100" alt="{{ $post->title }}"></td>
+                    <td>{{ $post->title }}</td>
+                    <td>
+                        <a href="{{ route('posts.edit', $post) }}" class="btn btn-sm btn-outline-success">Edit</a>
+                        <button class="btn btn-sm btn-outline-danger">Delete</button>
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="2">The posts is not found!</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 </div>
 @endsection
